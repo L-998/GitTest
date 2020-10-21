57e4a44ee3799753f17aa58320bbfa91d31d3b6e
Liu <286281900@qq.com>
2020-10-21 17:08:00 +0800

    尝试连接2

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * @author Lyh
+ * Hello
+ * @date 2020/10/21 17:05
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
